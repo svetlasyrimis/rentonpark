33e9e6dfabb17bdfa381f97af578e4254799e114
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-21 13:11:59 -0500

    ADD: Components admin layour footer and refact login

--- src/components/AdminLayout.js
+++ src/components/AdminLayout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../assets/styles_admin/bootstrap.css";
 import "../assets/styles_admin/waves.min.css";
+import "../assets/styles_admin/feather.css";
 import "../assets/styles_admin/style.css";
 import NavbarAdmin from "./NavbarAdmin";
 import FooterAdmin from "./FooterAdmin";
+import SidebarAdmin from "./SidebarAdmin";
 
 const AdminLayout = props => {
   return (
@@ -11,11 +13,18 @@ const AdminLayout = props => {
       <div className="loader-bg">
         <div className="loader-bar"></div>
       </div>
-      <div className="pcoded-overlay-box"></div>
-      <div className="pcoded-container navbar-wrapper">
-        <NavbarAdmin />
-        {props.children}
-        <FooterAdmin />
+      <div id="pcoded" className="pcoded">
+        <div className="pcoded-overlay-box"></div>
+        <div className="pcoded-container navbar-wrapper">
+          <NavbarAdmin />
+          <div className="pcoded-main-container">
+            <div className="pcoded-wrapper">
+              <SidebarAdmin />
+              {props.children}
+            </div>
+          </div>
+          <FooterAdmin />
+        </div>
       </div>
     </React.Fragment>
   );
