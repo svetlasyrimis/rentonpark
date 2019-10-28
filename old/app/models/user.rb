dd63d0c8aa0021719db16d392e86591f24e20aab
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-28 13:14:47 -0500

    DEL: Old project rails

--- old/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  enum sex: %i[masculino femenino]
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
