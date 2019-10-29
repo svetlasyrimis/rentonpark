6280362d509c06c7f492d9f48ab98121f12c6bf7
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-29 17:28:21 -0500

    UPD: Name api  to server

--- api/src/models/Tariff.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const tariffSchema = new mongoose.Schema({
-  title: String,
-  subtitle: String,
-  price: Number
-});
-
-module.exports = mongoose.model("Tariff", tariffSchema);
