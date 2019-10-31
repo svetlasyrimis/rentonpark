d8413bd731b1692315fe8734ef4f3f3e4dd7a248
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-31 11:25:40 -0500

    ADD: Post form contact

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
   name: String,
+  type: String,
   lastname: String,
   phone: String,
   email: String,
--- src/pages/School.js
+++ src/pages/School.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Loader from "../components/Loader";
 import Slider from "../components/Slider";
 import Section from "../components/Section";
+import FormContact from "../components/FormContact";
 import Background2 from "../assets/images/background_school2.jpg";
 
 const School = () => {
@@ -12,7 +13,6 @@ const School = () => {
   const [isDataSections, setIsDataSections] = useState(undefined);
   const [isLoadingSection, setIsLoadingSection] = useState(true);
 
-  var Recaptcha = require("react-recaptcha");
   const ImgStyle = {
     height: "150%"
   };
@@ -98,166 +98,7 @@ const School = () => {
             src={Background2}
           />
         </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 boxSchool">
-              <div className="feature feature-1 boxed">
-                <div className="text-center">
-                  <i className="ti-package icon"></i>
-                  <h5 className="uppercase">
-                    Si estas interesado en tomar clases, completa con tus datos
-                    la planilla que figura a continuación:
-                  </h5>
-                </div>
-                <div className="boxForm">
-                  <form
-                    className="form-school"
-                    role="form"
-                    method="post"
-                    action="./school/send"
-                  >
-                    <div className="row">
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label htmlFor="txtName"> Nombres </label>
-                          <input
-                            type="text"
-                            name="txtName"
-                            tabIndex="1"
-                            required=""
-                          />
-                        </div>
-                      </div>
-                      <div className="col-md-6">
-                        <div className="form-group">
-                          <label htmlFor="txtLast"> Apellido </label>
-                          <input
-                            type="text"
-                            name="txtLast"
-                            tabIndex="2"
-                            required=""
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-12">
-                        <div className="form-group">
-                          <label htmlFor="cmbDays">
-                            {" "}
-                            Dias (de martes a viernes){" "}
-                          </label>
-                          <div className="row">
-                            <div className="col-md-3">
-                              <div className="inputCheck">
-                                <input
-                                  type="checkbox"
-                                  value="2"
-                                  id="dayOne"
-                                  name="dayOne"
-                                  tabIndex="3"
-                                />
-                                <label
-                                  htmlFor="dayOne"
-                                  className="label-default"
-                                ></label>
-                              </div>
-                              <label htmlFor="">martes</label>
-                            </div>
-                            <div className="col-md-3">
-                              <div className="inputCheck">
-                                <input
-                                  type="checkbox"
-                                  value="3"
-                                  id="dayTwo"
-                                  name="dayTwo"
-                                  tabIndex="4"
-                                />
-                                <label
-                                  htmlFor="dayTwo"
-                                  className="label-default"
-                                ></label>
-                              </div>
-                              <label htmlFor="">miércoles</label>
-                            </div>
-                            <div className="col-md-3">
-                              <div className="inputCheck">
-                                <input
-                                  type="checkbox"
-                                  value="4"
-                                  id="dayThree"
-                                  name="dayThree"
-                                  tabIndex="5"
-                                />
-                                <label
-                                  htmlFor="dayThree"
-                                  className="label-default"
-                                ></label>
-                              </div>
-                              <label htmlFor="">jueves</label>
-                            </div>
-                            <div className="col-md-3">
-                              <div className="inputCheck">
-                                <input
-                                  type="checkbox"
-                                  value="5"
-                                  id="dayFour"
-                                  name="dayFour"
-                                  tabIndex="6"
-                                />
-                                <label
-                                  htmlFor="dayFour"
-                                  className="label-default"
-                                ></label>
-                              </div>
-                              <label htmlFor="">viernes</label>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-md-5">
-                        <div className="form-group">
-                          <label htmlFor="txtPhone"> Celular (Número) </label>
-                          <input
-                            type="text"
-                            name="txtPhone"
-                            tabIndex="9"
-                            required=""
-                          />
-                        </div>
-                      </div>
-                      <div className="col-md-7">
-                        <div className="form-group">
-                          <label htmlFor="txtMail"> Email </label>
-                          <input
-                            type="email"
-                            name="txtMail"
-                            tabIndex="10"
-                            required=""
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-xs-6">
-                        <Recaptcha
-                          sitekey="6LdRBQgUAAAAAI_YwLr-ESi-Lm3AdXKLuX7RE2Bl"
-                          render="explicit"
-                          hl={"es"}
-                        />
-                      </div>
-                      <div className="col-xs-6">
-                        <button type="submit">Enviar</button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <FormContact type={"escuelita"} />
       </section>
     </div>
   );
