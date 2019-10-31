1304eafca4cd58b1bd6403f9fe70c340f7ba6c18
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-31 13:42:50 -0500

    ADD: List to reservations in admin page

--- src/pages/Reservas.js
+++ src/pages/Reservas.js
@@ -1,6 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "../components/Loader";
 
 const Reservas = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [isData, setIsData] = useState(undefined);
+
+  const format_date = date => {
+    let new_date = new Date(date);
+    let minutes = new_date.getMinutes();
+    let hour = new_date.getHours();
+    return hour + ":" + minutes;
+  };
+
+  const only_date = date => {
+    let new_date = new Date(date);
+    let day = new_date.getDate();
+    let month = new_date.getMonth() + 1;
+    let year = new_date.getFullYear();
+    return day + "-" + month + "-" + year;
+  };
+
+  const fetchData = async () => {
+    setIsError(false);
+    await axios
+      .get("http://localhost:3001/api/reservations")
+      .then(res => {
+        console.log(res.data);
+        setIsData(res.data);
+      })
+      .catch(error => {
+        setIsError(error.response.data.message);
+        setIsLoading(false);
+      });
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <h1>Error....</h1>;
+  }
   return (
     <React.Fragment>
       <div className="row">
@@ -35,14 +82,27 @@ const Reservas = () => {
                       <th>Rider</th>
                       <th>Producto</th>
                       <th>Monto</th>
-                      <th>Estado</th>
-                      <th>Fecha Registro</th>
-                      <th>Mes</th>
-                      <th>Día</th>
-                      <th>Año</th>
+                      <th>Fecha</th>
+                      <th>Hora Inicio</th>
+                      <th>Hora Fin</th>
                     </tr>
                   </thead>
-                  <tbody></tbody>
+                  <tbody>
+                    {isData.map((reservation, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>
+                            {reservation.user.name} {reservation.user.lastname}
+                          </td>
+                          <td>{reservation.session.name}</td>
+                          <td>{reservation.total}</td>
+                          <td>{only_date(reservation.start)}</td>
+                          <td>{format_date(reservation.start)}</td>
+                          <td>{format_date(reservation.finish)}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
                 </table>
               </div>
             </div>
