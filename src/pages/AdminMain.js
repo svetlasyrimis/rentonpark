ced790320aef098e62288bef96cf8615152e431b
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-10-31 12:50:05 -0500

    ADD: Main view admin

--- src/pages/AdminMain.js
+++ src/pages/AdminMain.js
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "../components/Loader";
 
 const AdminMain = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [isData, setIsData] = useState(undefined);
+
+  const fetchData = async () => {
+    setIsError(false);
+    await axios
+      .get("http://localhost:3001/api/messages")
+      .then(res => {
+        setIsData(res.data);
+      })
+      .catch(error => {
+        setIsError(error.response.data.message);
+        setIsLoading(false);
+      });
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <h1>Error....</h1>;
+  }
+
   return (
-    <div className="col-lg-4 col-md-12">
-      <div className="card o-hidden">
-        <div className="card-footer">
-          <h4>Alvaro Alexander Palacios Carrillo</h4>
-          <p className="text-muted m-b-5">975012741</p>
-          <p className="text-muted">apalacioscarrillo@gmail.com</p>
-          <p className="text-muted text-center">Días de interés</p>
-          <div className="row">
-            <div className="col">
-              <h6>Lunes</h6>
-            </div>
-            <div className="col">
-              <h6>Miércoles</h6>
-            </div>
-            <div className="col">
-              <h6>Viernes</h6>
+    <React.Fragment>
+      <div className="row">
+        {isData.map((message, index) => (
+          <div className="col-lg-4 col-md-12" key={index}>
+            <div className="card o-hidden">
+              <div className="card-footer">
+                <h4>
+                  {message.name} {message.lastname}
+                </h4>
+                <p className="text-muted m-b-5">{message.phone}</p>
+                <p className="text-muted">{message.email}</p>
+                <p className="text-muted text-center">Días de interés</p>
+                <div className="row">
+                  {message.days.map((day, index) => {
+                    return (
+                      <div className="col" key={index}>
+                        <h6>{day}</h6>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
