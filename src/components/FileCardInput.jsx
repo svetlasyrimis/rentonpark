248404d2b9b6d5f7ac33ff1f91ed989f05c0a69d
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-11-06 13:50:28 -0500

    REFACT: Background admin view

--- src/components/FileCardInput.jsx
+++ src/components/FileCardInput.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loader from "../components/Loader";
 
-function FileCardInput({ name, handleChange, width_image, file }) {
+function FileCardInput({ name, width_image, type }) {
+  const [file, setFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [image, setImage] = useState({});
+
+  const handleChange = event => {
+    setFile(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const fetchData = async () => {
+    setIsError(false);
+    await axios
+      .get("http://localhost:3001/api/images_type/" + type)
+      .then(res => {
+        setImage(res.data[0]);
+      })
+      .catch(error => {
+        setIsError(error.response.data.message);
+        setIsLoading(false);
+      });
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <h1>Error....</h1>;
+  }
+
   return (
-    <div className="col-lg-6 text-center">
+    <div className="col-lg-6 text-center mt-5">
       <div className="row">
         <div className="col-lg-12">
           <div className="sub-title">{name}</div>
--- src/pages/Backgrounds.js
+++ src/pages/Backgrounds.js
@@ -1,53 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import FIleCardInput from "../components/FileCardInput";
+
 const Backgrounds = () => {
-  const [file, setFile] = useState(null);
-  const [file1, setFile1] = useState(null);
-  const [file2, setFile2] = useState(null);
-  const [file3, setFile3] = useState(null);
-  const [file4, setFile4] = useState(null);
-  const [file5, setFile5] = useState(null);
-  const [file6, setFile6] = useState(null);
-  const [file7, setFile7] = useState(null);
-  const [file8, setFile8] = useState(null);
-
-  const handleChange = event => {
-    setFile(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange1 = event => {
-    setFile1(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange2 = event => {
-    setFile2(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange3 = event => {
-    setFile3(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange4 = event => {
-    setFile4(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange5 = event => {
-    setFile5(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange6 = event => {
-    setFile6(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange7 = event => {
-    setFile7(URL.createObjectURL(event.target.files[0]));
-  };
-
-  const handleChange8 = event => {
-    setFile8(URL.createObjectURL(event.target.files[0]));
-  };
-
   const width_image = { maxWidth: "80%" };
+
   return (
     <React.Fragment>
       <div className="card">
@@ -57,58 +13,44 @@ const Backgrounds = () => {
         <div className="card-block">
           <div className="row">
             <FIleCardInput
-              name={"Home"}
-              handleChange={handleChange}
-              width_image={width_image}
-              file={file}
-            />
-            <FIleCardInput
               name={"Cablepark"}
-              handleChange={handleChange1}
               width_image={width_image}
-              file={file1}
+              type={"cablepark"}
             />
             <FIleCardInput
               name={"Escuelita"}
-              handleChange={handleChange2}
               width_image={width_image}
-              file={file2}
+              type={"escuelita"}
             />
             <FIleCardInput
               name={"Pez Volador"}
-              handleChange={handleChange3}
               width_image={width_image}
-              file={file3}
+              type={"pez_volador"}
             />
             <FIleCardInput
               name={"Cabañas y Hostel"}
-              handleChange={handleChange4}
               width_image={width_image}
-              file={file4}
+              type={"complex"}
             />
             <FIleCardInput
               name={"Camp & Grill "}
-              handleChange={handleChange5}
               width_image={width_image}
-              file={file5}
+              type={"camp"}
             />
             <FIleCardInput
               name={"Contacto"}
-              handleChange={handleChange6}
               width_image={width_image}
-              file={file6}
+              type={"contact"}
             />
             <FIleCardInput
               name={"Login"}
-              handleChange={handleChange7}
               width_image={width_image}
-              file={file7}
+              type={"login"}
             />
             <FIleCardInput
               name={"Register"}
-              handleChange={handleChange8}
               width_image={width_image}
-              file={file8}
+              type={"register"}
             />
           </div>
         </div>
