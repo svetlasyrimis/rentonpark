4d8a2f9aa360bb0d99fbcb4fdf1f0fe768b3f01b
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-11-22 01:42:26 -0500

    ADD: Get current reservations

--- server/src/controllers/reservationController.js
+++ server/src/controllers/reservationController.js
@@ -1,4 +1,5 @@
 const boom = require("boom");
+const moment = require("moment");
 
 // Get Data Models
 const Reservation = require("../models/Reservation");
@@ -28,6 +29,9 @@ exports.getSingleReservation = async (req, reply) => {
 // Add a new reservation
 exports.addReservation = async (req, reply) => {
   try {
+    req.body.start = moment(req.body.start);
+    req.body.finish = moment(req.body.finish);
+    console.log(req.body);
     const reservation = new Reservation(req.body);
     return reservation.save();
   } catch (err) {
@@ -56,6 +60,46 @@ exports.deleteReservation = async (req, reply) => {
     const id = req.params.id;
     const reservation = await Reservation.findByIdAndRemove(id);
     return reservation;
+  } catch (err) {
+    throw boom.boomify(err);
+  }
+};
+
+//Get reservations cablepark
+exports.reservationCablepark = async (req, reply) => {
+  try {
+    const date = new Date(req.query.vDate);
+    const reservations = await Reservation.find({
+      start: { $gte: date }
+    }).populate("session");
+    var final_reservations = [];
+    reservations.forEach(e => {
+      reservation = {
+        id: e._id,
+        color: "#b36500",
+        textColor: "#fafafa",
+        overlap: false,
+        start: e.start,
+        end: e.finish,
+        title: e.session.name + " - " + e.name,
+        vID: "5976",
+        vState: e.state,
+        vUser: "",
+        vProf: 2,
+        vType: "1",
+        vPrice: e.session.price,
+        vExtraW: e.session.extra_weekend,
+        vHash: "vGet"
+      };
+      final_reservations.push(reservation);
+    });
+    const response = {
+      tCustom: null,
+      tSessions: final_reservations,
+      tSpecial: null
+    };
+    return response;
+    // return {};
   } catch (err) {
     throw boom.boomify(err);
   }
